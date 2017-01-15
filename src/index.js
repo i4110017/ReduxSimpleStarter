7fce128e6c8d00338da97e5bf8c6ad4558e38587
Mintra Ruensuk <kikist@gmail.com>
2017-01-15 11:28:32 +0700

    Add search bar class component with input box and its state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyBIwWu4sxh_lKjzpxNvQjfArMJiAQm2hHo";
 // Create a new component. It should produce HTML.
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // The this component's generated HTML and put it on the page
